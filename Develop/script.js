ecc2dcc6c2adb062669b8b7e9f34cb7662b2af71
Alexa <alexandraws29@outlook.com>
2020-08-16 23:18:17 -0600

    editing generator code

--- Develop/script.js
+++ Develop/script.js
@@ -2,14 +2,15 @@
 
 
 // Arrays for available options
-var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var symbolsArray = ["!", "#", "$", "%", "&", "*", "+", "-", ".", ";", ":", ">", "<", "@", "/", "|", "^", "~", "{", "}", "`", "_", "[", "]"]
-var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var symbols = "0123456789"
+var numbers = "!@#$%^&*()_+-"
 
 
 // User input and prompts
-var passLength = prompt ("How many characters would you like to include between 8 and 25")
+var passLength = Number(window.prompt ("How many characters would you like to include (be sure to select more than 8)", ""))
+debugger;
 var incUppercase = confirm ("Include Uppercase Letters?")
 var incLowercase = confirm ("Include Lowercase Letters?")
 var incSymbols = confirm ("Include Symbols?")
@@ -18,12 +19,34 @@ var incNumbers = confirm ("Include Numbers?")
 
 
 
+Generating password
+  var included = "";
+  (incUppercase) ? included += uppercase : '';
+  (incLowercase) ? included += lowercase : '';
+  (incSymbols) ? included += symbols : '';
+  (incNumbers) ? included += numbers : '';
+
+
+
+function generatePassword (){
+  var result = ""
+  for(var i = 0; i < PassLength; i++) {
+    var inc = Math.floor(Math.random() * included.passlength)
+    result += included.charAt(inc);
+  }
+  
+  alert(result);
+}
 
 
 
 
 
 
+
+
+
+/*
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -38,3 +61,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
